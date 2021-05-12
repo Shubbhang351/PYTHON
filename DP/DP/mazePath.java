3f5ddc0400d135c5ea41262db85212fc2866f078
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-05-12 16:01:46 +0530

    351

--- DP/DP/mazePath.java
+++ DP/DP/mazePath.java
@@ -18,8 +18,54 @@ class MazePath{
         return ch + cv;
     }
 
+    public static int MPTD(int cr, int cc, int er, int ec, int[][] strg){
+        if(cr == er && cc == ec){
+            return 1;
+        }
+
+        if(cr > ec ||  cc > ec){
+            return 0;
+        }
+
+        if(strg[cr][cc] != 0){
+            return strg[cr][cc];
+        }
+
+        int ch = MPTD(cr, cc + 1, er, ec, strg);
+
+        int cv = MPTD(cr + 1, cc, er,ec, strg);
+
+        strg[cr][cc] = ch + cv;
+
+        return ch + cv;
+    }
+
+    public static int MPBU(int er, int ec){
+        int[][] strg = new int[er + 2][ec + 2];
+
+        
+
+        for(int row = er; row >= 0; row-- ){
+            for(int col = ec; col >= 0;col--){
+
+                if(row == er && col == ec){
+                    strg[row][col] = 1;
+                }
+                else{
+                    strg[row][col] = strg[row + 1][col] + strg[row][col + 1];
+                }
+            }
+        }
+
+        return strg[0][0];
+    }
+
     public static void main(String[] args) {
-        int n = 2;
-        System.out.println(MPRecursion(0, 0, n, n));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //System.out.println(MPRecursion(0, 0, n, n));
+        //System.out.println(MPTD(0, 0, n, n, new int[n + 1][n + 1]));
+
+        System.out.println(MPBU(n, n));
     }
 }

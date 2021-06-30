409b4a54e8c2007fbd424e5b43fb4e7b9e35464e
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-06-30 20:20:12 +0530

    351

--- final_prog/arrays/Array.java
+++ final_prog/arrays/Array.java
@@ -6,6 +6,17 @@ class Array {
 
     public static void main(String[] args) {
         int a[] = {10,52,30,90,80};
+        // System.out.println(maximum(a));
+        // System.out.println(find(a,80));
+
+        int arr[] = {10,30,50,80,90};
+
+        //System.out.println(BinarySearch(arr, 30));
+        System.out.println(Arrays.toString(arr));
+
+        updatedRotate(arr,4);
+        System.out.println(Arrays.toString(arr));
+
         
     }
 
@@ -15,5 +26,67 @@ class Array {
             maxi = Math.max(maxi,val);
         }
         return maxi;
+    }
+
+    public static int find(int a[],int item){
+        for(int i = 0;i < a.length;i++){
+            if(a[i] == item){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int BinarySearch(int arr[], int item){
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while(hi >= lo){
+            int mid = (hi + lo) / 2; 
+
+            if(arr[mid] == item)
+                return mid;
+            
+            else if(item > arr[mid]){
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void reverse(int a[],int si, int ei){
+        int i = si;
+        int j = ei;
+
+        while(j > i){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void rotate(int a[], int rot){
+        rot = rot % a.length;
+
+        for(int r = 1;r <= rot;r++){
+            // single rotation
+            int temp = a[a.length - 1];
+
+            for(int j = a.length - 1;j >= 1;j--){
+                a[j] = a[j - 1];
+            }
+            a[0] = temp;
+        }
+    }
+
+    public static void updatedRotate(int a[], int rot){
+        reverse(a,0,a.length - rot - 1);
+        reverse(a,a.length - rot - 1, a.length - 1);
+        reverse(a,0,a.length - 1);
     }
 }

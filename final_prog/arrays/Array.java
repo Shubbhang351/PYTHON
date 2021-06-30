b106a1df0c04321a9c07ecd0037fdfdb7edc37f1
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-06-30 21:57:50 +0530

    351

--- final_prog/arrays/Array.java
+++ final_prog/arrays/Array.java
@@ -5,19 +5,23 @@ class Array {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int a[] = {10,52,30,90,80};
+        //int a[] = {10,52,30,90,80};
         // System.out.println(maximum(a));
         // System.out.println(find(a,80));
 
         int arr[] = {10,30,50,80,90};
-
-        //System.out.println(BinarySearch(arr, 30));
-        System.out.println(Arrays.toString(arr));
-
-        updatedRotate(arr,4);
-        System.out.println(Arrays.toString(arr));
-
         
+        rotate(arr, 3);
+
+        System.out.println(Arrays.toString(arr));
+
+        int[] a = {10,30,52,80,90};
+        System.out.println(Arrays.toString(a));
+
+        updatedRotate(a, 3);
+
+        System.out.println(Arrays.toString(a));
+
     }
 
     public static int maximum(int a[]){
@@ -70,23 +74,34 @@ class Array {
         }
     }
 
+    public static void reverse(int a[]){
+        int i = 0;
+        int j = a.length - 1;
+
+        while(j > i){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
     public static void rotate(int a[], int rot){
+        
         rot = rot % a.length;
-
         for(int r = 1;r <= rot;r++){
-            // single rotation
             int temp = a[a.length - 1];
-
-            for(int j = a.length - 1;j >= 1;j--){
-                a[j] = a[j - 1];
-            }
-            a[0] = temp;
+        for(int j = a.length - 1;j >= 1;j--){
+            a[j] = a[j - 1];
+        }
+        a[0] = temp;
         }
     }
 
     public static void updatedRotate(int a[], int rot){
         reverse(a,0,a.length - rot - 1);
-        reverse(a,a.length - rot - 1, a.length - 1);
+        reverse(a,a.length - 1 - rot + 1, a.length - 1);
         reverse(a,0,a.length - 1);
     }
 }

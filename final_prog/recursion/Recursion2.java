64d0bc6f8e5fd0e27b4881f8c89a2d63d1cf7184
Shubhang <shubhang.verma_cs18@gla.ac.in>
2021-07-07 16:36:35 +0530

    351

--- final_prog/recursion/Recursion2.java
+++ final_prog/recursion/Recursion2.java
@@ -1,6 +1,7 @@
 class Recursion2{
     public static void main(String[] args) {
-        subsequence("abc", "", 0);
+        // subsequence("abc", "", 0);
+        permutation("abc", "", new boolean["abc".length()]);
     }
 
     public static void subsequence(String s , String ans, int i){
@@ -11,5 +12,20 @@ class Recursion2{
 
         subsequence(s, ans + s.charAt(i), i + 1);
         subsequence(s, ans, i + 1);
+    }
+
+    public static void permutation(String s, String ans,boolean[] choices){
+        if(ans.length() == s.length()){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i = 0;i < choices.length;i++){
+            if(choices[i] == false){
+                choices[i] = true;
+                permutation(s, ans + s.charAt(i), choices);
+                choices[i] = false;
+            }
+        }
     }
 }
